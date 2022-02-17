5b1e331bd23732d1aa9a34283fd7b8592fc245e7
Felix Medrano <sckrool@gmail.com>
2022-02-17 11:31:47 -0600

    Agregado / Eliminado
    Se agregan Img modificadas a tamaño a utilizar.
    
    Se eliminan las img de asset descargado
    
    Se cargan img a BD
    
    Se crea linea de codigo para probar el Random en las imagenes

--- Assets/Script/Card/CardController.cs
+++ Assets/Script/Card/CardController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardController : MonoBehaviour
 {
@@ -17,11 +18,17 @@ public class CardController : MonoBehaviour
     public int idFront;
     public int idBack;
 
+    public void SetImgFrontCard()
+    {
+        int id = Random.Range(0, (imgFrontPokerDB.frontPokerImgs.Length));
+        imgFront.sprite = imgFrontPokerDB.frontPokerImgs[id].imgFront;
+    }
+
     //TODO: Crear codigo para controlar la manipulacion de los sprites con un incremento automatico para mostrar X cantidad de cartas
 
     private void OnValidate()
     {
-        imgFront.sprite = imgFrontPokerDB.frontPokerImgs[idFront].imgFront;
+        SetImgFrontCard();
         imgBack.sprite = imgBackPokerDB.backPokerImgs[idBack].imgBack;
     }
 }
